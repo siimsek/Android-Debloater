3016ee0d0b9814e32687a60093e7f5146f1f28f8
Siimsek <62520983+siimsek@users.noreply.github.com>
2021-10-31 13:49:17 +0300

    Add files via upload

---
+++ NONROOT-Debloat.bat
@@ -0,0 +1,112 @@
+@echo off
+cd %cd%\adb
+adb shell pm disable-user --user 0 com.google.android.apps.wellbeing
+adb shell pm disable-user --user 0 com.android.storagemanager
+adb shell pm disable-user --user 0 com.android.printspooler
+adb shell pm disable-user --user 0 com.android.wallpaper.livepicker
+adb shell pm disable-user --user 0 com.google.android.as
+adb shell pm disable-user --user 0 com.google.android.tts
+adb shell pm disable-user --user 0 com.android.bookmarkprovider
+adb shell pm disable-user --user 0 com.android.emergency
+adb shell pm disable-user --user 0 com.google.android.gms.location.history
+adb shell pm disable-user --user 0 com.google.android.feedback
+adb shell pm disable-user --user 0 org.omnirom.omnijaws
+adb shell pm disable-user --user 0 com.android.managedprovisioning
+adb shell pm disable-user --user 0 com.google.android.projection.gearhead
+adb shell pm disable-user --user 0 com.android.dreams.phototable
+adb shell pm disable-user --user 0 com.google.android.googlequicksearchbox
+adb shell pm disable-user --user 0 com.android.bips
+adb shell pm disable-user --user 0 com.google.android.setupwizard
+adb shell pm disable-user --user 0 com.android.apps.tag
+adb shell pm disable-user --user 0 android.romstats
+adb shell pm disable-user --user 0 com.android.providers.calendar
+adb shell pm disable-user --user 0 com.tencent.soter.soterserver
+adb shell pm disable-user --user 0 com.android.cellbroadcastreceiver.module
+adb shell pm disable-user --user 0 com.android.providers.downloads.ui
+adb shell pm disable-user --user 0 com.google.android.apps.pixelmigrate
+adb shell pm disable-user --user 0 com.google.android.calendar
+adb shell pm disable-user --user 0 android.romstats
+adb shell pm disable-user --user 0 com.android.calendar
+adb shell pm disable-user --user 0 com.android.dreams.basic
+adb shell pm disable-user --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm disable-user --user 0 com.android.mms.service
+adb shell pm disable-user --user 0 com.android.mms
+adb shell pm disable-user --user 0 com.android.wallpaper.livepicker
+adb shell pm disable-user --user 0 com.google.android.feedback
+adb shell pm disable-user --user 0 com.google.android.marvin.talkback
+adb shell pm disable-user --user 0 com.google.android.music
+adb shell pm disable-user --user 0 com.google.android.printservice.recommendation
+adb shell pm disable-user --user 0 com.google.android.syncadapters.calendar
+adb shell pm disable-user --user 0 com.google.android.videos
+adb shell pm disable-user --user 0 com.google.ar.lens
+adb shell pm disable-user --user 0 com.mi.globalTrendNews
+adb shell pm disable-user --user 0 com.mi.webkit.core
+adb shell pm disable-user --user 0 com.mipay.wallet.id
+adb shell pm disable-user --user 0 com.mipay.wallet.in
+adb shell pm disable-user --user 0 com.miui.analytics
+adb shell pm disable-user --user 0 com.miui.bugreport
+adb shell pm disable-user --user 0 com.miui.extraphoto
+adb shell pm disable-user --user 0 com.miui.fm
+adb shell pm disable-user --user 0 com.miui.fmservice
+adb shell pm disable-user --user 0 com.miui.freeform
+adb shell pm disable-user --user 0 com.miui.hybrid
+adb shell pm disable-user --user 0 com.miui.hybrid.accessory
+adb shell pm disable-user --user 0 com.miui.mishare.connectivity
+adb shell pm disable-user --user 0 com.miui.miwallpaper.mars
+adb shell pm disable-user --user 0 com.miui.miwallpaper.earth
+adb shell pm disable-user --user 0 com.miui.msa.global
+adb shell pm disable-user --user 0 com.miui.nextpay
+adb shell pm disable-user --user 0 com.miui.phrase
+adb shell pm disable-user --user 0 com.miui.smsextra
+adb shell pm disable-user --user 0 com.miui.yellowpage
+adb shell pm disable-user --user 0 com.miui.wmsvc
+adb shell pm disable-user --user 0 com.xiaomi.joyose
+adb shell pm disable-user --user 0 com.xiaomi.glgm
+adb shell pm disable-user --user 0 com.xiaomi.midrop
+adb shell pm disable-user --user 0 com.xiaomi.mircs
+adb shell pm disable-user --user 0 com.xiaomi.payment
+adb shell pm disable-user --user 0 com.xiaomi.simactivate.service
+adb shell pm disable-user --user 0 com.samsung.android.email.provider
+adb shell pm disable-user --user 0 com.samsung.android.app.ledcoverdream
+adb shell pm disable-user --user 0 com.sec.android.cover.ledcover
+adb shell pm disable-user --user 0 com.sec.android.app.desktoplauncher
+adb shell pm disable-user --user 0 com.sec.android.app.withtv
+adb shell pm disable-user --user 0 com.samsung.android.app.memo
+adb shell pm disable-user --user 0 com.sec.spp.push
+adb shell pm disable-user --user 0 com.samsung.android.spay
+adb shell pm disable-user --user 0 com.samsung.android.voicewakeup
+adb shell pm disable-user --user 0 com.samsung.voiceserviceplatform
+adb shell pm disable-user --user 0 com.sec.android.sidesync30
+adb shell pm disable-user --user 0 com.samsung.android.hmt.vrsvc
+adb shell pm disable-user --user 0 com.samsung.android.app.vrsetupwizardstub
+adb shell pm disable-user --user 0 com.samsung.android.hmt.vrshell
+adb shell pm disable-user --user 0 com.android.exchange
+adb shell pm disable-user --user 0 com.samsung.groupcast
+adb shell pm disable-user --user 0 com.sec.android.service.health
+adb shell pm disable-user --user 0 com.sec.kidsplat.installer
+adb shell pm disable-user --user 0 com.sec.android.widgetapp.diotek.smemo
+adb shell pm disable-user --user 0 com.sec.android.app.translator
+adb shell pm disable-user --user 0 com.vlingo.midas
+adb shell pm disable-user --user 0 com.sec.readershub
+adb shell pm disable-user --user 0 com.sec.android.app.gamehub
+adb shell pm disable-user --user 0 com.sec.everglades.update
+adb shell pm disable-user --user 0 com.sec.everglades
+adb shell pm disable-user --user 0 tv.peel.samsung.app
+adb shell pm disable-user --user 0 com.sec.yosemite.phone
+adb shell pm disable-user --user 0 com.samsung.android.app.episodes
+adb shell pm disable-user --user 0 com.samsung.android.app.storyalbumwidget
+adb shell pm disable-user --user 0 com.samsung.android.tripwidget
+adb shell pm disable-user --user 0 com.samsung.android.service.travel
+adb shell pm disable-user --user 0 com.tripadvisor.tripadvisor
+adb shell pm disable-user --user 0 com.android.email
+adb shell pm disable-user --user 0 com.sec.android.app.ocr
+cls
+echo ____________________________________________________________
+echo |      ____       __    __            __           __   __ |
+echo |     / __ \___  / /_  / /___  ____ _/ /____  ____/ /  / / |
+echo |    / / / / _ \/ __ \/ / __ \/ __ `/ __/ _ \/ __  /  / /  |
+echo |   / /_/ /  __/ /_/ / / /_/ / /_/ / /_/  __/ /_/ /  /_/   |
+echo |  /_____/\___/_.___/_/\____/\__,_/\__/\___/\__,_/  (_)    |
+echo |__________________________________________________________|
+echo |                Made by @Siimsek on Telegram              |
+pause
